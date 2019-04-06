e7387d3b9c813300f3cb237e51773e54604bdc55
havard <havardnoren@gmail.com>
2019-04-06 14:05:43 +0200

    Initial Commit

---
+++ OnTheMap/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  OnTheMap
+//
+//  Created by mac on 4/6/19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OnTheMap/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OnTheMap
+//
+//  Created by mac on 4/6/19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
