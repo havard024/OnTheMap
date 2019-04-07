891117fe58108d7e99147177012a1b8c8525db87
havard <havardnoren@gmail.com>
2019-04-07 20:01:41 +0200

    populate pin list with student data

--- OnTheMap/Controller/PinListViewController.swift
+++ OnTheMap/Controller/PinListViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class PinListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension PinListViewController: UITableViewDelegate {
+    
+}
+
+extension PinListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return StudentModel.students.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let student = StudentModel.students[indexPath.count]
+        cell.textLabel?.text = "\(student.firstName!) \(student.lastName!)"
+        cell.imageView?.image = UIImage(named: "icon_pin")
+        
+        return cell
+    }
+    
+    
+}
--- OnTheMap/Model/Parse Responses/Student.swift
+++ OnTheMap/Model/Parse Responses/Student.swift
@@ -12,9 +12,11 @@ struct Student: Codable {
     let latitude: Float?
     let longitude: Float?
     let mediaURL: String?
+    let firstName: String?
+    let lastName: String?
     
     var isValid: Bool {
-        guard let _ = latitude, let _ = longitude, let _ = mediaURL else {
+        guard let _ = latitude, let _ = longitude, let _ = mediaURL, let _ = firstName, let _ = lastName else {
             return false
         }
         return true
